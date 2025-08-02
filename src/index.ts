32dacbc6774509cf9e964fa3b56fd2632fefe705
Dcyaprogrammer <dujustin6@gmail.com>
2025-08-02 20:54:33 +0800

    hello world

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Node.js!')
+})
+
+const server = serve(app)
+
+//graceful shutdown
+process.on('SIGINT', () => {
+  server.close()
+  process.exit(0)
+})
+
+process.on('SIGTERM', () => {
+  server.close((err) => {
+    if (err){
+      console.error(err)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
+})
